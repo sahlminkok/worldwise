72703393225328a32dc9976ba799eae93da1b386
sahlminkok <sahlminkok7@gmail.com>
2025-01-15 18:00:53 +0100

    Add login and app route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/product" element={<Product />} />
           <Route path="/pricing" element={<Pricing />} />
+          <Route path="/app" element={<AppLayout />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
