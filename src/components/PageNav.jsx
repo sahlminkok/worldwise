feebb533bebdbe9c91e38d4353ade1f00929f17c
sahlminkok <sahlminkok7@gmail.com>
2025-01-15 17:34:23 +0100

    Create page navigation component

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Homepage</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
