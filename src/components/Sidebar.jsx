3c9698ae13fbf39f88c3fa85aa8d370fe4781531
sahlminkok <sahlminkok7@gmail.com>
2025-01-16 21:49:46 +0100

    Create and style Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import Footer from "./Footer";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of countries</p>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
