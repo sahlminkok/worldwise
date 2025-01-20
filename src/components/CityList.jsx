876e3b6e2c90a26dc026425022f121705067868f
sahlminkok <sahlminkok7@gmail.com>
2025-01-20 11:09:19 +0100

    Handle case of cities is an empty array

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 import CityItem from "./CityItem";
 import Spinner from "./Spinner";
+import Message from "./Message";
 import styles from "./CityList.module.css";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on the map" />
+    );
 
   return (
     <ul className={styles.cityList}>
