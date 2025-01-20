d10a63b80013a32314faa32d729b6b42cf46c001
sahlminkok <sahlminkok7@gmail.com>
2025-01-20 11:03:44 +0100

    Accept cities prop in city list component and pass in city to its child component

---
+++ src/components/CityList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+CityList.propTypes = {
+  cities: PropTypes.array,
+  isLoading: PropTypes.bool,
+};
+
+export default CityList;
